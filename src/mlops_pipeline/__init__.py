8ac78a46b13b6920aad54fa78bbd08672f60ef8e
Maharshi Patel <pmaharshi999@gmail.com>
2026-09-23 11:17:17 +0200

    feat(pipeline): export multi-feature training APIs

--- src/mlops_pipeline/__init__.py
+++ src/mlops_pipeline/__init__.py
@@ -1,13 +1,27 @@
 from .core import LinearModel, mae, predict, psi, quality_gate, train
 from .pipeline import PipelineRun, run_regression_pipeline
+from .tabular import (
+    TabularLinearModel,
+    TabularPipelineRun,
+    predict_tabular,
+    run_tabular_regression_pipeline,
+    tabular_mae,
+    train_tabular,
+)
 
 __all__ = [
     "LinearModel",
     "PipelineRun",
+    "TabularLinearModel",
+    "TabularPipelineRun",
     "mae",
     "predict",
     "psi",
     "quality_gate",
     "run_regression_pipeline",
+    "predict_tabular",
+    "run_tabular_regression_pipeline",
+    "tabular_mae",
+    "train_tabular",
     "train",
 ]
